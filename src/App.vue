85433f85b2807ca6155cc657254b4369d9262dad
thatsShirleyLee <2690233316@qq.com>
2024-11-08 21:11:05 +0800

    write pages

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'App',
+})
+import {ref} from 'vue'
+import WeekDay from './components/WeekDay.vue';
+
+let startDate = ref('');
+let repeatNum = ref(2);
+let week = ref(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+const handleChange = (value: number) => {
+  console.log(value)
+}
+</script>
+
+<template>
+  <el-row><h3>Provider-end Schedule</h3></el-row>
+  <el-row>
+    <div style="display: flex;">
+      <div style="display: flex; flex-direction: column;">
+        <span class="demonstration">Start Date</span>
+        <div class="demo-date-picker">
+          <div class="block">
+            <el-date-picker
+              v-model="startDate"
+              format="MM/DD/YYYY"
+              value-format="MM/DD/YYYY"
+              type="date"
+              placeholder="Pick a day"
+            />
+          </div>
+        </div>
+      </div>
+      <div style="display: flex; flex-direction: column; margin-left: 20px;">
+        <span class="demonstration">Repeat</span>
+        <el-input-number v-model="repeatNum" :min="1" :max="10" @change="handleChange" /> 
+      </div>
+    </div>
+  </el-row>
+  <el-row style="margin-top: 20px;">
+    <el-col :span="12">
+      <div v-for="dayName in week" style="border: 1px solid #cbd5da; height: 400px; width: 80%;">
+        <div style="background-color: #edeef0; border-bottom: 1px solid #cbd5da; text-indent: 1.5em">Availability Setting</div>
+        <WeekDay :dayname="dayName"></WeekDay>
+      </div>
+    </el-col>
+    <el-col :span="12">
+      xxx
+    </el-col>
+  </el-row>
+  <el-divider />
+  <el-row><h3>Patient-end Show</h3></el-row>
+  <el-row>
+    <el-col :span="24">
+      xxx
+    </el-col>
+  </el-row>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/WeekDay.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'WeekDay',
+})
+defineProps(['dayname']);
+import {ref} from 'vue'
+let startTime = ref('')
+let endTime = ref('')
+</script>
+
+<template>
+  <div class="weekDay">
+    <span>{{dayname}}</span>
+    <div class="demo-time-range">
+    <el-time-select
+      v-model="startTime"
+      style="width: 200px"
+      start="00:00"
+      step="00:15"
+      end="23:59"
+      format="hh:mm A"
+    />
+    <span style="color: #cbd5da; margin: 5px;">-</span>
+    <el-time-select
+      v-model="endTime"
+      style="width: 200px"
+      start="00:00"
+      step="00:15"
+      end="23:59"
+      format="hh:mm A"
+    />
+    <el-button link style="color: #6e97ba; margin-left: 20px;">
+      <el-icon :size="20"><CirclePlus /></el-icon>
+      <span style="font-size: 1em;">Add New</span>
+    </el-button>
+  </div>
+</div>
+</template>
+
+<style scoped>
+  .weekDay {
+    text-indent: 1.5em;
+    border-bottom: 1px solid #cbd5da;
+    display: flex;
+    align-items: center;
+    height: 60px;
+  }
+  .weekDay span {
+    font-size: medium;
+    font-weight: 500;
+  }
+</style>
