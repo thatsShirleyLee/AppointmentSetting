ea43d6d04352cff63988f353cbb73bea26bdbc55
thatsShirleyLee <2690233316@qq.com>
2024-11-08 22:40:46 +0800

    update

---
+++ src/components/TimeChunk.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'TimeChunk',
+})
+defineProps(['startTime', 'endTime'])
+</script>
+
+<template>
+  <!-- <div style="background-color: #ebf1f5;"> -->
+    <span>06:00AM - 06:00PM</span>
+    <!-- <el-icon style="margin-left: 5px;"><Close :color="`#d25a58`"/></el-icon> -->
+  <!-- </div> -->
+</template>
+
+<style scoped>
+  span {
+    color: #4675a4;
+    background-color: aquamarine;
+    font-size: 0.5em;
+  }
+</style>
--- src/components/WeekDay.vue
+++ src/components/WeekDay.vue
@@ -10,30 +10,30 @@ let endTime = ref('')
 
 <template>
   <div class="weekDay">
-    <span>{{dayname}}</span>
+    <span style="width: 160px;">{{dayname}}</span>
     <div class="demo-time-range">
-    <el-time-select
-      v-model="startTime"
-      style="width: 200px"
-      start="00:00"
-      step="00:15"
-      end="23:59"
-      format="hh:mm A"
-    />
-    <span style="color: #cbd5da; margin: 5px;">-</span>
-    <el-time-select
-      v-model="endTime"
-      style="width: 200px"
-      start="00:00"
-      step="00:15"
-      end="23:59"
-      format="hh:mm A"
-    />
-    <el-button link style="color: #6e97ba; margin-left: 20px;">
+      <el-time-select
+        v-model="startTime"
+        style="width: 200px"
+        start="00:00"
+        step="00:15"
+        end="23:59"
+        format="hh:mm A"
+      />
+      <span style="color: #cbd5da; margin: 5px;">-</span>
+      <el-time-select
+        v-model="endTime"
+        style="width: 200px"
+        start="00:00"
+        step="00:15"
+        end="23:59"
+        format="hh:mm A"
+      />
+  </div>
+    <el-button link style="color: #6e97ba; margin-left: 60px;">
       <el-icon :size="20"><CirclePlus /></el-icon>
       <span style="font-size: 1em;">Add New</span>
     </el-button>
-  </div>
 </div>
 </template>
 
@@ -41,9 +41,13 @@ let endTime = ref('')
   .weekDay {
     text-indent: 1.5em;
     border-bottom: 1px solid #cbd5da;
+    border-right: 1px solid #cbd5da;
+    border-left: 1px solid #cbd5da;
     display: flex;
     align-items: center;
+    justify-content: flex-start;
     height: 60px;
+    width: 860px;
   }
   .weekDay span {
     font-size: medium;
